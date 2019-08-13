368e445a5ad5810a4ee631587fe711b3cdb8c05c
Michael <michael.currin@gmail.com>
2019-08-13 22:46:37 +0200

    feat: Update script

---
+++ pyprojecttemplate/script_sample.py
@@ -0,0 +1,49 @@
+"""
+Sample python script.
+
+This is a reference for laying out a Python script and be deleted.
+
+It contains the following, layed out according to PEP-8 naming and spacing:
+- imports
+- global variables
+- functions
+- main function
+- call to main function, using command-line arguments and ignoring the path to the
+    file itself at index 0.
+"""
+import sys
+
+
+SOME_GLOBAL = "My global"
+
+
+def foo():
+    """
+    Foo docstring here.
+    """
+    pass
+
+
+def bar(a, b):
+    """
+    Bar docstring here.
+    """
+    return a + b
+
+
+def main(args):
+    """
+    Main command-line function.
+    """
+    print("It works!")
+    print("Args:")
+    print(args)
+    print(SOME_GLOBAL)
+    
+    foo()
+    x = bar(1, 2)
+    print(x)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
