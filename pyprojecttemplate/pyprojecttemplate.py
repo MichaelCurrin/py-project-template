2d513dfeed1c20a07e2b9ef556b065c1c7e1ca9f
Michael <michael.currin@gmail.com>
2019-08-13 22:47:11 +0200

    feat: Create main script

---
+++ pyprojecttemplate/pyprojecttemplate.py
@@ -0,0 +1,15 @@
+"""
+Main application file.
+"""
+import sys
+
+
+def main(args):
+    """
+    Main command-line function.
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
