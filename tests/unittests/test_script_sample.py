04dfd28d6849d90730453eafbac690d6a9c6a0b0
Michael Currin <michael.currin@gmail.com>
2020-04-08 09:13:01 +0200

    test: Add tests

---
+++ tests/unittests/test_script_sample.py
@@ -0,0 +1,13 @@
+"""
+Unit tests module.
+"""
+from pyprojecttemplate.script_sample import foo, bar
+
+
+def test_foo():
+    assert foo() is None
+
+
+def test_bar():
+    assert bar(0, 0) == 0
+    assert bar(5, 8) == 13
