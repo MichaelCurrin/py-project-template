5d1019267eece3ef29e705f44df4c09c56151def
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-11-02 19:54:44 +0200

    docs: update __main__.py

--- htmlscreenshot/__main__.py
+++ htmlscreenshot/__main__.py
@@ -1,9 +1,15 @@
+"""
+HTML Screenshot application.
+"""
 import sys
 
 from . import lib, scrape
 
 
 def process(path_str: str) -> None:
+    """
+    Scrape all URLs for given text file path.
+    """
     urls = lib.read(path_str)
 
     print(f"Found URLs: {len(urls)}")
@@ -19,13 +25,15 @@ def process(path_str: str) -> None:
 
 
 def main(args: list[str]) -> None:
+    """
+    Command-line entry-point.
+    """
     if not args:
         print("Required arg: PATH")
         print("Provide a path to a text file of one URL per line")
         sys.exit(0)
 
     path_str = args.pop(0)
-
     process(path_str)
 
 
