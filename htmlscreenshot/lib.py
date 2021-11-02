751790cd41014b382ea32c0a707791001443f944
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-11-02 19:52:34 +0200

    update lib.py

--- htmlscreenshot/lib.py
+++ htmlscreenshot/lib.py
@@ -1,3 +1,6 @@
+"""
+Lib module.
+"""
 import re
 from pathlib import Path
 
@@ -5,11 +8,14 @@ PATTERN = re.compile(r"[\W_]+")
 
 
 def read(path_str: str) -> list[str]:
+    """
+    Return the lines of a given text file.
+    """
     path = Path(path_str)
 
     assert path.exists(), f"Unable to read: {path}"
 
-    with open(path) as f_in:
+    with open(path, encoding="utf-8") as f_in:
         text = f_in.read()
         urls = text.splitlines()
 
@@ -17,6 +23,11 @@ def read(path_str: str) -> list[str]:
 
 
 def slugify(value: str) -> str:
+    """
+    Convert value to a slug - safe for URLs and filenames.
+
+    Strips out any non-ASCII characters.
+    """
     value = value.encode("ascii", errors="replace").decode()
 
     return PATTERN.sub("-", value)
