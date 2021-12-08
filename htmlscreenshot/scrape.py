afe768228eca1e74141b9e8c2bfb2b6c90eda8f3
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-12-08 22:44:53 +0200

    refactor: update scrape.py

--- htmlscreenshot/scrape.py
+++ htmlscreenshot/scrape.py
@@ -3,20 +3,15 @@ Scrape module.
 
 Importable functions and CLI tool for converting a single URL into an image.
 """
-import datetime
 import sys
-from pathlib import Path
 from time import sleep
 
 from selenium import webdriver
 
 from . import lib
-
-OUT_DIR = Path(__file__).parent / "var"
+from .lib import ADD_DATETIME_DEFAULT, DATETIME_FORMAT, OUT_DIR
 
 WAIT_S = 3
-DATETIME_FORMAT = "%Y-%m-%d--%H:%m"
-ADD_DATETIME_DEFAULT = True
 
 driver = None
 
@@ -67,30 +62,13 @@ def load(url: str) -> str:
     return title
 
 
-def _make_filename(name: str, add_datetime: bool) -> str:
-    """
-    Convert an image name into a suitable filename for an image.
-    """
-    filename = lib.slugify(name)
-
-    if not filename.endswith(".png"):
-        filename = f"{filename}.png"
-
-    if add_datetime:
-        now = datetime.datetime.now()
-        dt_str = now.strftime(DATETIME_FORMAT)
-        filename = f"{dt_str}-{filename}"
-
-    return filename
-
-
 def save(name: str, add_datetime: bool) -> None:
     """
     Take a screenshot of the current page and save it with the given name.
 
     The PNG prefix is added here internally because the webdriver requires it.
     """
-    filename = _make_filename(name, add_datetime)
+    filename = lib.make_filename(name, ".png", add_datetime)
 
     out_path = OUT_DIR / filename
     driver.get_screenshot_as_file(str(out_path))
